bc4877d08c11311b8eed6659e50add5ead749770
Camilo S. Ibagon <brandon.sarmiento@unp.gov.co>
2024-11-05 16:36:54 -0500

    Feat(DatosBasicos):Se agregaron estilos y boton

--- src/shared/components/DatosBasicos.tsx
+++ src/shared/components/DatosBasicos.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { FaUser, FaAlignJustify, FaUsers, FaUserShield, FaUserTag } from "react-icons/fa6";
+import { FaUser, FaUsers, FaUserTag } from "react-icons/fa6";
 import { SubtituloForm } from "eco-unp/ui";
-import { Form, FormGroup, FormSelect } from "react-bootstrap";
+import { Form, Table, Button } from "react-bootstrap";
+import { IoIosArrowDropdownCircle, IoIosArrowDroprightCircle } from "react-icons/io";
 
-const DatosBasicos: React.FC = () =>{
+const DatosBasicos: React.FC = () => {
     // Simulando datos obtenidos desde la base de datos
     const [formData] = useState({
         fechaLlegada: "2024-10-29",
@@ -34,70 +35,103 @@ const DatosBasicos: React.FC = () =>{
         nombreConsejoComunitario: ""
     });
 
-    const [tipoEstudio, setTipoEstudio] = useState(""); // Controlar la selección del tipo de estudio
+    // Estados para controlar la visibilidad de cada sección, inicialmente en 'false'
+    const [showDatosPersonales, setShowDatosPersonales] = useState(false);
+    const [showGrupoPoblacional, setShowGrupoPoblacional] = useState(false);
+    const [showFactorDiferencial, setShowFactorDiferencial] = useState(false);
 
     return (
         <>
-            {/* Sección: Datos Personales */}
-            <SubtituloForm subtitulo={"Datos Básicos"} icon={FaUser} />
-            <Form className="text-start">
-                <ul className="list-unstyled">
-                    <li><b>Fecha de Llegada:</b> {formData.fechaLlegada}</li>
-                    <li><b>Nombre Completo:</b> {formData.nombreCompleto}</li>
-                    <li><b>Tipo de Documento:</b> {formData.tipoDocumento}</li>
-                    <li><b>Número de Identificación:</b> {formData.numeroIdentificacion}</li>
-                    <li><b>Sexo:</b> {formData.sexo}</li>
-                    <li><b>Departamento:</b> {formData.departamento}</li>
-                    <li><b>Municipio:</b> {formData.municipio}</li>
-                    <li><b>Dirección de Residencia:</b> {formData.direccion}</li>
-                    <li><b>Teléfono:</b> {formData.telefono}</li>
-                </ul>
-            </Form>
+            <div className='item_container'>
+                <div className="tittle-container-modal">
+                    <SubtituloForm subtitulo={"Datos Básicos"} icon={FaUser} />
+                    <Button variant="link" onClick={() => setShowDatosPersonales(!showDatosPersonales)}>
+                        {showDatosPersonales ? <IoIosArrowDropdownCircle className="boton-desplegable" /> : <IoIosArrowDroprightCircle className="boton-desplegable" />}
+                    </Button>
+                </div>
+                {showDatosPersonales && (
+                    <Form className="text-start">
+                        <Table striped bordered hover>
+                            <tbody>
+                                <tr><td><b>Fecha de Llegada</b></td><td>{formData.fechaLlegada}</td></tr>
+                                <tr><td><b>Nombre Completo</b></td><td>{formData.nombreCompleto}</td></tr>
+                                <tr><td><b>Tipo de Documento</b></td><td>{formData.tipoDocumento}</td></tr>
+                                <tr><td><b>Número de Identificación</b></td><td>{formData.numeroIdentificacion}</td></tr>
+                                <tr><td><b>Sexo</b></td><td>{formData.sexo}</td></tr>
+                                <tr><td><b>Departamento</b></td><td>{formData.departamento}</td></tr>
+                                <tr><td><b>Municipio</b></td><td>{formData.municipio}</td></tr>
+                                <tr><td><b>Dirección de Residencia</b></td><td>{formData.direccion}</td></tr>
+                                <tr><td><b>Teléfono</b></td><td>{formData.telefono}</td></tr>
+                            </tbody>
+                        </Table>
+                    </Form>
+                )}
+            </div>
 
-            {/* Sección: Grupo Poblacional */}
-            <SubtituloForm subtitulo={"Grupo Poblacional"} icon={FaUsers} />
-            <Form className="text-start">
-                <ul className="list-unstyled">
-                    <li><b>Tipo de Grupo:</b> {formData.tipoGrupo}</li>
-                    <li><b>Subcategoría:</b> {formData.subcategoria}</li>
-                    <li><b>Otros Grupos Poblacionales:</b> {formData.otrosGrupos}</li>
-                </ul>
-            </Form>
+            <div className='item_container'>
+                <div className="tittle-container-modal">
+                    <SubtituloForm subtitulo={"Grupo Poblacional"} icon={FaUsers} />
+                    <Button variant="link" onClick={() => setShowGrupoPoblacional(!showGrupoPoblacional)}>
+                        {showGrupoPoblacional ? <IoIosArrowDropdownCircle className="boton-desplegable" /> : <IoIosArrowDroprightCircle className="boton-desplegable" />}
+                    </Button>
+                </div>
+                {showGrupoPoblacional && (
+                    <Form className="text-start">
+                        <Table striped bordered hover>
+                            <tbody>
+                                <tr><td><b>Tipo de Grupo</b></td><td>{formData.tipoGrupo}</td></tr>
+                                <tr><td><b>Subcategoría</b></td><td>{formData.subcategoria}</td></tr>
+                                <tr><td><b>Otros Grupos Poblacionales</b></td><td>{formData.otrosGrupos}</td></tr>
+                            </tbody>
+                        </Table>
+                    </Form>
+                )}
+            </div>
 
-            {/* Sección: Factor Diferencial */}
-            <SubtituloForm subtitulo={"Factor Diferencial"} icon={FaUserTag} />
-            <Form className="text-start">
-                <ul className="list-unstyled">
-                    <li><b>Género:</b> {formData.genero}</li>
-                    <li><b>Orientación Sexual:</b> {formData.orientacionSexual}</li>
-                    <li><b>Factor Diferencial:</b> {formData.factorDiferencial}</li>
-                    <li><b>Personas a Cargo:</b> {formData.personasACargo}</li>
-                    <li><b>¿Posee algún tipo de discapacidad?:</b> {formData.discapacidad}</li>
-                    {formData.discapacidad === "Sí" && (
-                        <li><b>Tipo de Discapacidad:</b> {formData.tipoDiscapacidad}</li>
-                    )}
-                    <li><b>¿Se autorreconoce como miembro de algún grupo étnico?:</b> {formData.autorreconocidoEtnico}</li>
-                    {formData.autorreconocidoEtnico === "Sí" && (
-                        <>
-                            <li><b>Grupo Étnico:</b> {formData.grupoEtnico}</li>
-                            {formData.grupoEtnico === "Indígena" && (
-                                <>
-                                    <li><b>Etnia o Grupo Indígena:</b> {formData.etniaIndigena}</li>
-                                    <li><b>Resguardo:</b> {formData.resguardo}</li>
-                                    <li><b>Comunidad dentro del Resguardo:</b> {formData.comunidadResguardo}</li>
-                                    <li><b>Parcialidad:</b> {formData.parcialidad}</li>
-                                    <li><b>Comunidad sin registro:</b> {formData.comunidadSinRegistro}</li>
-                                </>
-                            )}
-                            {(formData.grupoEtnico === "Negro" || formData.grupoEtnico === "Afrocolombiano") && (
-                                <li><b>Consejo Comunitario:</b> {formData.nombreConsejoComunitario}</li>
-                            )}
-                        </>
-                    )}
-                </ul>
-            </Form>
-
-            
- </>)
+            <div className='item_container'>
+                <div className="tittle-container-modal">
+                    <SubtituloForm subtitulo={"Factor Diferencial"} icon={FaUserTag} />
+                    <Button variant="link" onClick={() => setShowFactorDiferencial(!showFactorDiferencial)}>
+                        {showFactorDiferencial ? <IoIosArrowDropdownCircle className="boton-desplegable" /> : <IoIosArrowDroprightCircle className="boton-desplegable" />}
+                    </Button>
+                </div>
+                {showFactorDiferencial && (
+                    <Form className="text-start">
+                        <Table striped bordered hover>
+                            <tbody>
+                                <tr><td><b>Género</b></td><td>{formData.genero}</td></tr>
+                                <tr><td><b>Orientación Sexual</b></td><td>{formData.orientacionSexual}</td></tr>
+                                <tr><td><b>Factor Diferencial</b></td><td>{formData.factorDiferencial}</td></tr>
+                                <tr><td><b>Personas a Cargo</b></td><td>{formData.personasACargo}</td></tr>
+                                <tr><td><b>¿Posee algún tipo de discapacidad?</b></td><td>{formData.discapacidad}</td></tr>
+                                {formData.discapacidad === "Sí" && (
+                                    <tr><td><b>Tipo de Discapacidad</b></td><td>{formData.tipoDiscapacidad}</td></tr>
+                                )}
+                                <tr><td><b>¿Se autorreconoce como miembro de algún grupo étnico?</b></td><td>{formData.autorreconocidoEtnico}</td></tr>
+                                {formData.autorreconocidoEtnico === "Sí" && (
+                                    <>
+                                        <tr><td><b>Grupo Étnico</b></td><td>{formData.grupoEtnico}</td></tr>
+                                        {formData.grupoEtnico === "Indígena" && (
+                                            <>
+                                                <tr><td><b>Etnia o Grupo Indígena</b></td><td>{formData.etniaIndigena}</td></tr>
+                                                <tr><td><b>Resguardo</b></td><td>{formData.resguardo}</td></tr>
+                                                <tr><td><b>Comunidad dentro del Resguardo</b></td><td>{formData.comunidadResguardo}</td></tr>
+                                                <tr><td><b>Parcialidad</b></td><td>{formData.parcialidad}</td></tr>
+                                                <tr><td><b>Comunidad sin registro</b></td><td>{formData.comunidadSinRegistro}</td></tr>
+                                            </>
+                                        )}
+                                        {(formData.grupoEtnico === "Negro" || formData.grupoEtnico === "Afrocolombiano") && (
+                                            <tr><td><b>Consejo Comunitario</b></td><td>{formData.nombreConsejoComunitario}</td></tr>
+                                        )}
+                                    </>
+                                )}
+                            </tbody>
+                        </Table>
+                    </Form>
+                )}
+            </div>
+        </>
+    );
 };
+
 export default DatosBasicos;
